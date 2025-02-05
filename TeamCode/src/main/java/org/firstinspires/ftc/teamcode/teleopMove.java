304bfe16b17a401a754416ebb75cc5daffcdfb61
StPatsRobotics <jj.fischer10@gmail.com>
2025-02-05 12:20:46 -0600

    Made a button to automatically clip a block

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleopMove.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleopMove.java
@@ -3,7 +3,12 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 @TeleOp (name = "teleopMove")
 public class teleopMove extends LinearOpMode{
@@ -15,8 +20,23 @@ public class teleopMove extends LinearOpMode{
     public DcMotor motorSlide;
     public Servo servoSampleClaw;
     public Servo servoSpecimenClaw;
+    private IMU imu;
 
+    private ElapsedTime runtime = new ElapsedTime();
 
+    static final double COUNTS_PER_INCH = 44.62;
+    static final double DRIVE_SPEED = 0.6;
+    static  final double ARM_SPEED = 0.4;
+    static final double SLIDE_SPEED = 0.7;
+    static final double SLOW_SPEED = 0.4;
+    static final int YAW_PRECISION = 500;
+
+    private int idealPosMotorFL = 0;
+    private int idealPosMotorFR = 0;
+    private int idealPosMotorBL = 0;
+    private int idealPosMotorBR = 0;
+    YawPitchRollAngles robotOrientation;
+    double yaw;
     public double speedMode = 1;
     public double servoSamplePos = 0.025;
     public double servoSpecimenPos = 0.5;
@@ -24,6 +44,7 @@ public class teleopMove extends LinearOpMode{
     boolean gamepad2ButtonA = false;
     boolean gamepad2ButtonY = false;
     boolean gamepad2ButtonB = false;
+    boolean gamepad1ButtonA = false;
     boolean controlledServo = false;
 
     boolean gamepad2ButtonYTriggered = false;
@@ -117,8 +138,10 @@ public class teleopMove extends LinearOpMode{
             boolean needToScale = false;
             boolean prevGamepad2ButtonA = gamepad2ButtonA;
             boolean prevGamepad2ButtonB = gamepad2ButtonB;
+            boolean prevGamepad1ButtonA = gamepad1ButtonA;
             gamepad2ButtonA = gamepad2.a;
             gamepad2ButtonB = gamepad2.b;
+            gamepad1ButtonA = gamepad1.a;
             for (double power : powers) {
                 if (Math.abs(power) > 1) {
                     needToScale = true;
@@ -231,6 +254,12 @@ public class teleopMove extends LinearOpMode{
                 motorArm.setPower(gamepad2.right_trigger - gamepad2.left_trigger);
             }
 
+            if (gamepad1ButtonA && !prevGamepad1ButtonA && !gamepad1.start) {
+                setSlidePos(SLIDE_SPEED, 1850, 3);
+                setArmPos(ARM_SPEED, 435, 1, true);
+                setSlidePos(0.5, 1350, 1);
+            }
+
             telemetry.addData("Right Bumper", gamepad2.right_bumper);
             telemetry.addData("Left Bump", gamepad2.left_bumper);
             telemetry.addData("Controlling Servo: ", !controlledServo ? "Sample" : "Specimen");
@@ -247,6 +276,253 @@ public class teleopMove extends LinearOpMode{
             telemetry.update();
         }
 
+    }
+    public void tankDrive(double speed, double leftInches, double rightInches, double angle, int precision, double timeoutS) {
+        int newFLtarget;
+        int newBLtarget;
+        int newFRtarget;
+        int newBRtarget;
+
+        // Ensure that the OpMode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newFLtarget = idealPosMotorFL += (int)(leftInches * COUNTS_PER_INCH);
+            newBLtarget = idealPosMotorBL += (int)(leftInches * COUNTS_PER_INCH);
+            newFRtarget = idealPosMotorFR += (int)(rightInches * COUNTS_PER_INCH);
+            newBRtarget = idealPosMotorBR += (int)(rightInches * COUNTS_PER_INCH);
+            motorFL.setTargetPosition(newFLtarget);
+            motorBL.setTargetPosition(newBLtarget);
+            motorFR.setTargetPosition(newFRtarget);
+            motorBR.setTargetPosition(newBRtarget);
+
+            // Turn On RUN_TO_POSITION
+            motorBL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorFL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorBR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorFR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            motorBL.setPower(Math.abs(speed));
+            motorFL.setPower(Math.abs(speed));
+            motorFR.setPower(Math.abs(speed));
+            motorBR.setPower(Math.abs(speed));
+
+            while (opModeIsActive() && (runtime.seconds() < timeoutS) && (motorFL.isBusy() && motorBL.isBusy() && motorFR.isBusy() && motorBR.isBusy())) {
+                if (!Double.isNaN(angle)) {
+                    robotOrientation = imu.getRobotYawPitchRollAngles();
+                    yaw = robotOrientation.getYaw(AngleUnit.DEGREES);
+                    double yawError = Math.abs(((540 + yaw - angle) % 360) - 180);
+
+                    motorBL.setPower(clamp(Math.abs(speed) + yawError / precision, 0, 1));
+                    motorFL.setPower(clamp(Math.abs(speed) - yawError / precision, 0, 1));
+                    motorFR.setPower(clamp(Math.abs(speed) + yawError / precision, 0, 1));
+                    motorBR.setPower(clamp(Math.abs(speed) - yawError / precision, 0, 1));
+
+                    telemetry.addData("yawError", yawError);
+                    telemetry.update();
+                }
+            }
+
+            // Stop all motion;
+            motorBL.setPower(0);
+            motorFL.setPower(0);
+            motorFR.setPower(0);
+            motorBR.setPower(0);
+
+            sleep(50);
+
+
+            motorBL.setPower(0.2);
+            motorFL.setPower(0.2);
+            motorFR.setPower(0.2);
+            motorBR.setPower(0.2);
+
+
+            while (opModeIsActive() && (runtime.seconds() < timeoutS) && (motorFL.isBusy() && motorBL.isBusy() && motorFR.isBusy() && motorBR.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Running to",  " %7d :%7d :%7d :%7d", newFLtarget, newBLtarget, newFRtarget, newBRtarget);
+                telemetry.addData("Currently at",  " at %7d :%7d :%7d :%7d", motorFL.getCurrentPosition(), motorBL.getCurrentPosition(), motorFR.getCurrentPosition(), motorBR.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Stop all motion;
+            motorBL.setPower(0);
+            motorFL.setPower(0);
+            motorFR.setPower(0);
+            motorBR.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            motorBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            motorFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            motorBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            sleep(50);   // optional pause after each move.
+        }
+    }
+    public void sideDrive(double speed, double sideDistance, double timeoutS) {
+        int newFLtarget;
+        int newBLtarget;
+        int newFRtarget;
+        int newBRtarget;
+
+        // Ensure that the OpMode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newFLtarget = idealPosMotorFL += (int)(sideDistance * COUNTS_PER_INCH);
+            newBLtarget = idealPosMotorBL -= (int)(sideDistance * COUNTS_PER_INCH);
+            newFRtarget = idealPosMotorFR -= (int)(sideDistance * COUNTS_PER_INCH);
+            newBRtarget = idealPosMotorBR += (int)(sideDistance * COUNTS_PER_INCH);
+            motorFL.setTargetPosition(newFLtarget);
+            motorBL.setTargetPosition(newBLtarget);
+            motorFR.setTargetPosition(newFRtarget);
+            motorBR.setTargetPosition(newBRtarget);
+
+            // Turn On RUN_TO_POSITION
+            motorBL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorFL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorBR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorFR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            motorBL.setPower(Math.abs(speed));
+            motorFL.setPower(Math.abs(speed));
+            motorFR.setPower(Math.abs(speed));
+            motorBR.setPower(Math.abs(speed));
+
+
+            while (opModeIsActive() && (runtime.seconds() < timeoutS) && (motorFL.isBusy() && motorBL.isBusy() && motorFR.isBusy() && motorBR.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Running to",  " %7d :%7d :%7d :%7d", newFLtarget, newBLtarget, newFRtarget, newBRtarget);
+                telemetry.addData("Currently at",  " at %7d :%7d :%7d :%7d", motorFL.getCurrentPosition(), motorBL.getCurrentPosition(), motorFR.getCurrentPosition(), motorBR.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Stop all motion;
+            motorBL.setPower(0);
+            motorFL.setPower(0);
+            motorFR.setPower(0);
+            motorBR.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            motorBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            motorFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            motorBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            sleep(100);   // optional pause after each move.
+        }
+    }
+    public void setArmPos(double speed, int armPos, double timeoutS, boolean holdPos) {
+
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            motorArm.setTargetPosition(armPos);
+
+            // Turn On RUN_TO_POSITION
+            motorArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            motorArm.setPower(Math.abs(speed));
+
+
+            while (opModeIsActive() && (runtime.seconds() < timeoutS) && (motorArm.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Running to",  " %7d", armPos);
+                telemetry.addData("Currently at",  " at %7d", motorArm.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Turn off RUN_TO_POSITION and stop motion
+            if (!holdPos) {
+                motorArm.setPower(0);
+                motorArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            }
+
+            sleep(100);   // optional pause after each move.
+        }
+    }
+    public void setSlidePos(double speed, int slidePos, double timeoutS) {
+
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            motorSlide.setTargetPosition(slidePos);
+
+            // Turn On RUN_TO_POSITION
+            motorSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            motorSlide.setPower(Math.abs(speed));
+
+
+            while (opModeIsActive() && (runtime.seconds() < timeoutS) && (motorSlide.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Running to",  " %7d", slidePos);
+                telemetry.addData("Currently at",  " at %7d", motorSlide.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Stop all motion;
+            motorSlide.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            motorSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            sleep(100);   // optional pause after each move.
+        }
+    }
+    public void turnToAngle(double speed, double angle, double timeoutS) {
+        int startPositionBL = motorBL.getCurrentPosition();
+        int startPositionFL = motorFL.getCurrentPosition();
+        int startPositionFR = motorFR.getCurrentPosition();
+        int startPositionBR = motorBR.getCurrentPosition();
+        runtime.reset();
+
+        robotOrientation = imu.getRobotYawPitchRollAngles();
+        yaw = robotOrientation.getYaw(AngleUnit.DEGREES);
+        double yawDifference = Math.abs(((540 + yaw - angle) % 360) - 180);
+
+        while (opModeIsActive() && (runtime.seconds() < timeoutS) && (Math.abs(yawDifference) > 10)) {
+            robotOrientation = imu.getRobotYawPitchRollAngles();
+            yaw = robotOrientation.getYaw(AngleUnit.DEGREES);
+            yawDifference = Math.abs(((540 + yaw - angle) % 360) - 180);
+
+            motorBL.setPower((yawDifference / Math.abs(yawDifference)) * -Math.abs(speed));
+            motorFL.setPower((yawDifference / Math.abs(yawDifference)) * -Math.abs(speed));
+            motorFR.setPower((yawDifference / Math.abs(yawDifference)) * Math.abs(speed));
+            motorBR.setPower((yawDifference / Math.abs(yawDifference)) * Math.abs(speed));
+
+            telemetry.addData("yaw", yaw);
+            telemetry.addData("yawDifference", yawDifference);
+            telemetry.update();
+        }
+
+        motorBL.setPower(0);
+        motorFL.setPower(0);
+        motorFR.setPower(0);
+        motorBR.setPower(0);
+
+        sleep(100);
+
+        idealPosMotorBL += motorBL.getCurrentPosition() - startPositionBL;
+        idealPosMotorFL += motorFL.getCurrentPosition() - startPositionFL;
+        idealPosMotorFR += motorFR.getCurrentPosition() - startPositionFR;
+        idealPosMotorBR += motorBR.getCurrentPosition() - startPositionBR;
+    }
+    public double clamp(double value, double min, double max) {
+        return Math.max(min, Math.min(value, max));
     }
 }
 
